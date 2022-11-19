fd621fcff131419a3980e335bf2a54e2fa69e732
Ömer Faruk KÖSE <omerfarukkose@Omer-MacBook-Air.local>
2022-11-19 19:22:52 +0300

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "main-color": "#00072d",
+      },
+    },
+  },
+  plugins: [],
+}
